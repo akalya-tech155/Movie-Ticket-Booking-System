dfec862592776d8344e9176dc291a179d56b0b24
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-20 18:52:17 +0530

    Added movie selection feature

---
+++ pages/MoviePage.ts
@@ -0,0 +1,45 @@
+/*
+
+Scenario
+
+User should
+
+Open website
+Select city
+Select movie
+Verify movie name
+
+Example
+
+Open BookMyShow
+
+↓
+
+Select Chennai
+
+↓
+
+Select Movie
+
+↓
+
+Leo
+
+↓
+
+Verify
+Movie Selected Successfully
+
+
+Page Object
+HomePage
+
+selectCity()
+
+MoviePage
+
+selectMovie()
+
+verifyMovie()
+
+*/
