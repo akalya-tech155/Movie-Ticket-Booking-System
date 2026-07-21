439d5050581c3a0736053e64776b29655f48e2f7
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-21 19:00:33 +0530

    Fixed double booking issue

---
+++ tests/booking.spec.ts
@@ -0,0 +1,23 @@
+/*
+
+Seat
+
+A5
+
+is already booked.
+
+Another user tries to book
+
+A5
+
+again.
+
+Expected
+
+Seat Already Booked
+
+Assertion
+
+expect(errorMessage).toContain("Seat already booked");
+
+*/
